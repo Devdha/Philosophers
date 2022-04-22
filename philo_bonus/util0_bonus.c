fbdb016d46955b85d752abecde9de553f8fbe3cb
p0tt3r-iOS <france998811@gmail.com>
2022-04-22 19:41:12 +0900

    Bonus: edit atoi

--- philo_bonus/util0_bonus.c
+++ philo_bonus/util0_bonus.c
@@ -6,7 +6,7 @@
 /*   By: dha <dha@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 21:30:04 by dha               #+#    #+#             */
-/*   Updated: 2022/04/21 16:33:08 by dha              ###   ########seoul.kr  */
+/*   Updated: 2022/04/22 15:01:52 by dha              ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,6 @@ int	ft_atoi(const char *str)
 	unsigned long	tot;
 	int				sign;
 
-	sign = 1;
 	tot = 0;
 	sign = 1 - ((*str == '-') << 1);
 	str += (*str == '-' || *str == '+');
